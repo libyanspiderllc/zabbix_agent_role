9d1580826ae4ca8f27912eb541754ab1f973f9f2
ahmed-faraj <a.faraj@email.ly>
2025-05-11 12:05:24 +0200

    this commit is to include the configuration needed for spamexpert plugin and allowremote domain and encoded cron jobs configuration and scripts

---
+++ files/bin/allowremotedomains.sh
@@ -0,0 +1,2 @@
+grep '^allowremotedomains' /var/cpanel/cpanel.config | cut -d '=' -f2
+
---
+++ files/bin/find_encoded_cron_jobs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# The following command will find users who have encoded cron jobs
+for user in $(awk -F: '$3 >= 1000' /etc/passwd | cut -f1 -d:); do crontab -u "$user" -l 2>/dev/null | grep -E 'base64|gzinflate|str_rot13' >/dev/null && echo "$user"; done
+~
