722f6a69cfc6ab971ae80867786fd022c9647016
ahmed-faraj <a.faraj@email.ly>
2025-07-21 15:42:44 +0200

    Updated the suspend_email.py to be compatible with python3 and to suspend login as well

--- files/bin/suspend_email.py
+++ files/bin/suspend_email.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 import sys
 import subprocess
-import urllib
+import urllib.parse
 import json
 
 def main():
 	if len(sys.argv) < 2:
-		print "Argument missing: Supply an Email address"
+		print ("Argument missing: Supply an Email address")
 		exit(1)
 
 	email = sys.argv[1]
@@ -15,21 +15,25 @@ def main():
 	try:
 		domain = email.split('@')[1]
 	except Exception:
-		print 'Unable to parse email address'
+		print ('Unable to parse email address')
 		exit(1)
 
 	command = "grep -R %s /var/cpanel/users | grep -v system | gawk -F':' '{print $1}' | head -n 1 | xargs egrep ^USER | gawk -F'=' '{print $2}'" % domain
 	email_owner,error  = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+	suspend_outgoing = "uapi --user=%s --output=json Email suspend_outgoing email=%s" % (email_owner.strip('\n'), urllib.parse.quote_plus(email))
+	suspend_login = "uapi --user=%s --output=json Email suspend_login email=%s" % (email_owner.strip('\n'), urllib.parse.quote_plus(email))
+	result_outgoing, error_outgoing = subprocess.Popen(suspend_outgoing, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+	result_login, error_login = subprocess.Popen(suspend_login, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
-	api_call = "uapi --user=%s --output=json Email suspend_outgoing email=%s" % (email_owner.strip('\n'), urllib.quote_plus(email))
-	result,error  = subprocess.Popen(api_call, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 	try:
-		result = json.loads(result)
-		status = result['result']['status']
-		if status == 1:
-			exit(0)
-		else:
-			exit(1)
+            data_outgoing = json.loads(result_outgoing)
+            data_login = json.loads(result_login)
+            status_outgoing = data_outgoing['result']['status']
+            status_login = data_login['result']['status']
+            if status_outgoing == 1 and status_login == 1:
+                exit(0)
+            else:
+                exit(1)
 	except Exception as e:
 		exit(1)
 
